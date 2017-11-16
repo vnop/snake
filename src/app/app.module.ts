2313c058d0960cdb7687a3457ba538c3a5df1fa6
Vandeth Nop <vnop14@gmail.com>
2017-11-15 17:19:23 -0800

    Created game area

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { GardenComponent } from './garden/garden.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GardenComponent
   ],
   imports: [
     BrowserModule
