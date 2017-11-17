8d3d89b9d9596c1d99273b8a6b2aebd3148ec515
Vandeth Nop <vnop14@gmail.com>
2017-11-16 22:53:50 -0800

    Fruit aligns with snake

--- src/app/fruit/fruit.component.ts
+++ src/app/fruit/fruit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-fruit',
@@ -7,18 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FruitComponent implements OnInit {
 
-  constructor() { }
+  constructor(private data: DataService) { }
   ctx: any;
   x: number;
   y: number;
 
   ngOnInit() {
-    let canvas = document.getElementById("garden");
-    this.ctx = canvas.getContext("2d");
-    this.ctx.fillStyle = "red";
-    this.x = 50;
-    this.y = 100;
-    this.ctx.fillRect(this.x, this.y, 25, 25);
+    // let canvas = document.getElementById("garden");
+    // this.ctx = canvas.getContext("2d");
+    // this.ctx.fillStyle = "red";
+    // this.x = 50;
+    // this.y = 100;
+    // this.ctx.fillRect(this.x, this.y, 25, 25);
+    //on init give data position of fruit
+    this.relocate();
   }
 
+  relocate() {
+    console.log('FROM FRUIT COMP');
+    // update new position to data
+    this.data.fruitPos.x = Math.floor(Math.random() * 601);
+    this.data.fruitPos.y = Math.floor(Math.random() * 601);
+    this.data.fruitPos.eatten = false;
+  }
 }
